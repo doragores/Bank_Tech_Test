9b6d836c095e1425df86858b87c144dc55dfe08d
Dora Gores <d.lopesgores@gmail.com>
2023-03-20 16:33:29 +0000

    Changed current tests to display date 1st and then ammount. Also added test to satisfy statement method

--- BankTest.test.js
+++ BankTest.test.js
@@ -12,22 +12,35 @@ describe('Account', () => {
   });
 
   it('should handle a single deposit', () => {
-    account.deposit(1000, "10/01/2023");
+    account.deposit("10/01/2023", 1000);
 
     expect(account.balance).toEqual(1000)
   });
 
   it('should handle multiple deposits', () => {
-    account.deposit(1000, "10/01/2023");
-    account.deposit(2000, "13/01/2023");
+    account.deposit("10/01/2023", 1000);
+    account.deposit("13/01/2023", 2000);
 
     expect(account.balance).toEqual(3000);
-  })
+  });
 
   it('should handle a single withdrawal', () => {
-    account.deposit(1000, "10/01/2023");
-    account.deposit(2000, "13/01/2023");
-    account.withdraw(500, "14/01/2023");
+    account.deposit("10/01/2023", 1000);
+    account.deposit("13/01/2023", 2000);
+    account.withdraw("14/01/2023", 500);
     expect(account.balance).toEqual(2500);
-  })
-})
+  });
+
+  it('should print a statement', () => {
+    account.deposit("10/01/2023", 1000);
+    account.deposit("13/01/2023", 2000);
+    account.withdraw("14/01/2023", 500);
+
+    expect(account.statement()).toEqual(
+      "date || credit || debit || balance\n" +
+      "14/01/2023 || 500.00 || 2500.00\n" +
+      "13/01/2023 || 2000.00 || || 3000.00\n" +
+      "10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
+});
