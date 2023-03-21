1ae863fd1d696347fffb36816253c9e5c0c9f9c6
Dora Gores <d.lopesgores@gmail.com>
2023-03-21 11:24:14 +0000

    Refactored for loop to make it simpler to read; Added comments to explain statment method & commented Example use

--- BankTest.js
+++ BankTest.js
@@ -15,15 +15,22 @@ class Account {
   }
 
   statement() {
-    let statement = "date || credit || debit || balance";
-    for (let i = this.transactions.length - 1; i >= 0; i--) {
-    const transaction = this.transactions[i];
+    let statement = "date || credit || debit || balance"; // Adding header to statement
+
+    // Looping through transactions array in reverse order 
+    for (const transaction of this.transactions.reverse()) {
+    
+    // Checking if each transaction has a truthy value for each credit and debit
+    //If it does, converts it to 2 decimals, otherwise returns an empty string
     const creditAmount = transaction.credit ? transaction.credit.toFixed(2) : '';
     const debitAmount = transaction.debit ? transaction.debit.toFixed(2) : '';
+    
+    // Adding each date and transaction bellow statement header
     statement += `\n${transaction.date} ||${creditAmount ? ` ${creditAmount}` : ''} ||${debitAmount ? ` ${debitAmount}` : ''} || ${transaction.balance.toFixed(2)}`;
     }
+
     return statement;
-  } 
+  }
 };
 
 module.exports = Account;
@@ -31,9 +38,9 @@ module.exports = Account;
 
 // Example use
 
-const account = new Account();
-account.deposit("10/01/2023", 1000)
-account.deposit("13/01/2023", 2000)
-account.withdraw("14/01/2023", 500)
+// const account = new Account();
+// account.deposit("10/01/2023", 1000)
+// account.deposit("13/01/2023", 2000)
+// account.withdraw("14/01/2023", 500)
 
-console.log(account.statement());
+// console.log(account.statement());
