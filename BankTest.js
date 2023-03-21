8413e748e8766f9b8e74824e3d4a9978d845f62b
Dora Gores <d.lopesgores@gmail.com>
2023-03-21 11:00:29 +0000

    Added date to methods, transactions to constructor and wrote statement method

--- BankTest.js
+++ BankTest.js
@@ -1,17 +1,39 @@
 class Account {
   constructor() {
     this.balance = 0;
+    this.transactions = []
   }
 
-  deposit(amount) {
+  deposit(date, amount) {
     this.balance += amount;
+    this.transactions.push({ date, credit: amount, debit: '', balance: this.balance})
   }
 
-  withdraw(amount) {
+  withdraw(date, amount) {
     this.balance -= amount;
+    this.transactions.push({ date, credit: '', debit: amount, balance: this.balance})
   }
-}
 
-
+  statement() {
+    let statement = "date || credit || debit || balance";
+    for (let i = this.transactions.length - 1; i >= 0; i--) {
+    const transaction = this.transactions[i];
+    const creditAmount = transaction.credit ? transaction.credit.toFixed(2) : '';
+    const debitAmount = transaction.debit ? transaction.debit.toFixed(2) : '';
+    statement += `\n${transaction.date} ||${creditAmount ? ` ${creditAmount}` : ''} ||${debitAmount ? ` ${debitAmount}` : ''} || ${transaction.balance.toFixed(2)}`;
+    }
+    return statement;
+  } 
+};
 
 module.exports = Account;
+
+
+// Example use
+
+const account = new Account();
+account.deposit("10/01/2023", 1000)
+account.deposit("13/01/2023", 2000)
+account.withdraw("14/01/2023", 500)
+
+console.log(account.statement());
